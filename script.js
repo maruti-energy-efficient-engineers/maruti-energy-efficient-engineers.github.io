0089b57a6a62c06640993232a91d0a0a1bead2a7
maruti-energy-efficient-engineers <maruti.energy.efficient@gmail.com>
2025-09-30 12:26:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// Simple alert on successful form submission
+document.getElementById("consultForm").addEventListener("submit", function () {
+  alert("Thank you! Your request has been received. We'll contact you shortly.");
+});
